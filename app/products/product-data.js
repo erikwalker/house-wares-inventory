e7140bbd875cff1585964304962a9b1c1f746a36
erik walker <erik.walker2016@gmail.com>
2019-02-10 17:37:33 -0500

    -commit of ui changes for house wares inventory -includes some minor refactoring

---
+++ app/products/product-data.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_ITEMS = [{
+        id: 1,
+        name: 'Fry Pan',
+        description: 'used to fry or brown food',
+        price: 15.99
+    }, {
+        id: 2,
+        name: 'Boiling Pot',
+        description: 'used to boil or steam vegetables or rice',
+        price: 20.99
+    }, {
+        id: 3,
+        name: 'Cutting Board',
+        description: 'used to cut up vegetables or meat',
+        price: 6.99
+    }, {
+        id: 4,
+        name: 'Food Processor',
+        description: 'used to grind vegatables into sauce',
+        price: 12.99
+    }];
+//# sourceMappingURL=product-data.js.map
---
+++ app/products/product-data.ts
@@ -0,0 +1,23 @@
+import { Product } from './product';
+
+export const PRODUCT_ITEMS: Product[] = [{
+    id: 1,
+    name: 'Fry Pan',
+    description: 'used to fry or brown food',
+    price: 15.99
+  }, {
+    id: 2,
+    name: 'Boiling Pot',
+    description: 'used to boil or steam vegetables or rice',
+    price: 20.99
+  }, {
+    id: 3,
+    name: 'Cutting Board',
+    description: 'used to cut up vegetables or meat',
+    price: 6.99
+  },{
+    id: 4,
+    name: 'Food Processor',
+    description: 'used to grind vegatables into sauce',
+    price: 12.99
+  }]
